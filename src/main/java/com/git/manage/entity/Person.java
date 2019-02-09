96c31d07ae6d16c27841eb2d5c5abccac68bacb9
zhuhaolin <18575624813@163.com>
2019-02-10 00:03:17 +0800

    创建项目

---
+++ src/main/java/com/git/manage/entity/Person.java
@@ -0,0 +1,24 @@
+package com.git.manage.entity;
+
+/**
+ * 
+ * @author Îíº¥
+ *
+ */
+public class Person {
+	private String name;
+	private Integer age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getAge() {
+		return this.age;
+	}
+}
